5728f2981aeda00ad325f0eb214d71c0c84158d0
MuskanNadeem28 <za7589506@gmail.com>
2025-04-21 15:29:05 +0500

    Add files via upload

---
+++ Assignment_00_07/00_intro_python/06_square_number.py
@@ -0,0 +1,13 @@
+def main():
+
+    number = float(input("Type a number to see its square: \033[0m"))
+    
+   
+    square = number * number
+    
+    print(f"{number} squared is {square}")
+
+
+
+if __name__ == '__main__':
+    main()
